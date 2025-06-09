d4fb402429ae3aa75c0a326c77e47c0a2fef0f31
codejsandcss <deansandria5@gmail.com>
2025-06-09 12:18:24 +0700

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,24 @@
+function loadAds(param) {
+  let adUrl;
+
+  if (param === 'A') {
+    adUrl = "https://codejsandcss.github.io/functions/ads-n1.html";
+  } else if (param === 'B') {
+    adUrl = "https://codejsandcss.github.io/functions/ads-n2.html";
+  } else if (param === 'RANDOM') {
+    const ads = [
+      "https://codejsandcss.github.io/functions/ads-n1.html",
+      "https://codejsandcss.github.io/functions/ads-n1.html"
+    ];
+    adUrl = ads[Math.floor(Math.random() * ads.length)];
+  }
+
+  if (adUrl) {
+    const iframe = document.createElement("iframe");
+    iframe.src = adUrl;
+    iframe.width = "300";
+    iframe.height = "250";
+    iframe.style.border = "none";
+    document.body.appendChild(iframe); 
+  }
+}
